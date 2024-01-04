a0e259c2488b7e3da20f9cbda0d1f1b2427f1270
DanielBonnery <daniel.bonnery@gmail.com>
2024-01-04 16:47:00 +0100

    update readme rmd

--- R/get_data.R
+++ R/get_data.R
@@ -2,6 +2,6 @@ get_data<-function(){
   dest_file=tempfile(fileext="zip")
 "https://www.aeaweb.org/doi/10.1257/mac.20180090.data"|>
   downloader::download(dest_file)
-if(!exists(tempdir())){dir.create(tempdir(),recursive = TRUE)}
+if(!dir.exists(tempdir())){dir.create(tempdir(),recursive = TRUE)}
 dest_file|>utils::unzip(exdir = tempdir())
 file.path(tempdir(),"data","data_var","data.csv")|>read.csv()}
